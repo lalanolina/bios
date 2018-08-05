8e27ea89856324da7a624202d5f9af0c9cbad5f8
lalanolina <lalanolina@gmail.com>
2018-08-05 19:22:57 -0300

    Construir html e css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Meu primeiro WebApp incrível!' });
+});
+
+
+/* GET helicopterson page. */
+router.get('/helicopterson', function(req, res, next) {
+  res.render('bio');
 });
 
 module.exports = router;
